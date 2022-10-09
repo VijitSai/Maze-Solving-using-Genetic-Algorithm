4201b5d8dee297123bba083b5096c72af7b141e2
jaya-shankar <bjayashankar2102@gmail.com>
2022-10-09 14:17:12 +0200

    add task3 algorithm

--- maze.py
+++ maze.py
@@ -1,74 +1,133 @@
 import random
+from unittest import result
+import numpy as np
+import math
 
 class Maze:
     
-    def __init__(self, height, width, obstacles_percent = 0.5, solutions_count = 1, solution_in_steps = None):
+    def __init__(self, height, width):
         self.height = height
         self.width = width
-        self.solutions_count = solutions_count
-        self.obstacles_count = int(self.height*self.width*obstacles_percent)
         self.solutions = []
         self.empty = set()
         self.obstacle_loc = set()
-        self.solution_in_steps = solution_in_steps if solution_in_steps else self.height+self.width-1
+        self.goal_state = None
+        self.start_state = None
+        self.start_state = (0, 0)
+        self.solution_in_steps = 0
         self.generate_maze()
     
     def generate_maze(self):
+
+        a_height = 2*self.height+1
+        a_width = 2*self.width+1
         
-        self.maze = [[0 for i in range(self.width)] for j in range(self.height)]
-        self.generate_solutions()
-        obs_count = 0
-        for i in range(self.obstacles_count):
-            x = random.randint(0, self.height-1)
-            y = random.randint(0, self.width-1)
-            if x*self.width+y not in self.empty and (x, y) != (0, 0) and (x, y) != (self.height-1, self.width-1):
-                self.maze[x][y] = 1
-                self.obstacle_loc.add((x*self.width+y))
-                obs_count += 1
+        self.maze = []
+        for i in range(a_height):
+            if i%2 == 0:
+                self.maze.append([1]*a_width)
             else:
-                i-=1
-        self.maze[0][0] = 2
-        self.maze[self.height-1][self.width-1] = 3
-        print('Obstacles count: ', obs_count)
-        
-    def generate_solutions(self):
-        count = 0
-        while(count < self.solutions_count):
-            path = [(0, 0)]
-            pos = (0, 0)
-            steps_count = 0
-            while(pos != (self.height-1, self.width-1) and steps_count < self.solution_in_steps):
-                if pos[0] == self.height-1:
-                    pos = (pos[0], pos[1]+1)
-                elif pos[1] == self.width-1:
-                    pos = (pos[0]+1, pos[1])
-                else:
-                    pos = random.choice([(pos[0]+1, pos[1]), (pos[0], pos[1]+1)])
-                steps_count += 1
-                path.append(pos)
-                self.empty.add(pos[0]*self.width+pos[1])
-            if pos == (self.height-1, self.width-1):
-                self.solutions.append(path)
-                count += 1
-            
-            
+                row = []
+                for j in range(a_width):
+                    if j%2 == 0:
+                        row.append(1)
+                    else:
+                        row.append(0)
+                self.maze.append(row)
+        self.maze[1][1] = 2
+        self.start_state = (1, 1)
+        self.create_paths()
+        self.set_goal_state()
     
-    def show_maze(self,show_path = False):
-        for i in range(self.height):
-            for j in range(self.width):
+    def create_paths(self):
+        path = [(0, 0)]
+        pos = (1, 1)
+        steps_count = 0
+        visited = {(1, 1)}
+        while(len(visited)<self.height*self.width):
+            possible_moves = self.generate_moves(pos,visited)
+            if possible_moves == []:
+                pos = path.pop()
+                if pos == (1, 1):
+                    break
+                steps_count -= 1
+                continue
+            pos,wall = random.choice(possible_moves) 
+            steps_count += 1
+            path.append(pos)
+            visited.add(pos)
+            self.maze[wall[0]][wall[1]] = 0
+        self.solution_in_steps = steps_count
+    
+    def set_goal_state(self):
+        a_height = 2*self.height+1
+        a_width = 2*self.width+1
+        self.goal_state = (a_height-2, a_width-2)
+        self.maze[a_height-2][a_width-2] = 3
+             
+    def generate_moves(self, pos, visited):
+        possible_moves = []
+        a_height = 2*self.height+1
+        a_width = 2*self.width+1
+        # moving up
+        if pos[0] > 1 and (pos[0]-2, pos[1]) not in visited :
+            possible_moves.append([(pos[0]-2, pos[1]), (pos[0]-1, pos[1])])
+        # moving down
+        if pos[0] < a_height-2 and (pos[0]+2, pos[1]) not in visited:
+            possible_moves.append([(pos[0]+2, pos[1]), (pos[0]+1, pos[1])])
+        # moving left
+        if pos[1] > 1 and (pos[0], pos[1]-2) not in visited:
+            possible_moves.append([(pos[0], pos[1]-2), (pos[0], pos[1]-1)])
+        # moving right
+        if pos[1] < a_width-2 and (pos[0], pos[1]+2) not in visited :
+            possible_moves.append([(pos[0], pos[1]+2), (pos[0], pos[1]+1)])
+        return possible_moves
+  
+    def show_path(self, path):
+        
+        a_width = 2*self.width+1
+        pos = self.start_state
+        self.empty = set()
+        for next in path:
+            if next == 0:
+                pos = (pos[0], pos[1])
+            elif next == 1:
+                if self.maze[pos[0]-1][pos[1]] == 1:
+                    continue
+                pos = (pos[0]-1, pos[1])
+            elif next == 2:
+                if self.maze[pos[0]][pos[1]-1] == 1:
+                    continue
+                pos = (pos[0], pos[1]-1)
+            elif next == 3:
+                if self.maze[pos[0]+1][pos[1]] == 1:
+                    continue
+                pos = (pos[0]+1, pos[1])
+            elif next == 4:
+                if self.maze[pos[0]][pos[1]+1] == 1:
+                    continue
+                pos = (pos[0], pos[1]+1)
+            if pos == self.goal_state:
+                break
+            self.empty.add(pos[0]*a_width+pos[1])
+        print(self.__str__(show_path = True))
+    def __str__(self,show_path = False):
+        result = ''
+        a_width = 2*self.width+1
+        for i in range(len(self.maze)):
+            for j in range(len(self.maze[0])):
                 if self.maze[i][j] == 3:
-                    print('|E|', end='')
-                elif i*self.width+j in self.empty and show_path:
-                    print('|0|', end='')
+                    result += 'E '
+                elif i*a_width+j in self.empty and show_path:
+                    result += '0 '
                 elif self.maze[i][j] == 0:
-                    print('| |', end='')
+                    result += '  '
                 elif self.maze[i][j] == 1:
-                    print('|#|', end='')
+                    result += '# '
                 elif self.maze[i][j] == 2:
-                    print('|S|', end='')
-            print()
+                    result += 'S '
+            result += '\n'
+        return result
 
-maze = Maze(10, 10, 0.8, 1)
-
-maze.show_maze(True)
- 
+# maze = Maze(10,10)
+# print(maze)
